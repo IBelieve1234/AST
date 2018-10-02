edca3515c02c38a545d52f40a1c199ec8235794b
StevenQiao <983466967@qq.com>
2018-10-02 20:21:30 +0800

    read third try

--- main.cpp
+++ main.cpp
@@ -7,18 +7,27 @@ using namespace std;
 int main()
 {
     fstream inFile;
+    fstream outFile;
     inFile.open("../ast1.c.001t.tu",ios::in);
+    outFile.open("../result.txt",ios::out);
     string bufLine;
     string nextLine;
 
     getline( inFile,bufLine);//The firstLine
 
+
     while(!inFile.eof())
     {
        getline(inFile,nextLine);
+       for(int i=0;i<nextLine.length();i++)
+       {
+           if(nextLine[i]=='\r')
+               nextLine[i]=' ';
+       }
        if(nextLine[0]=='@')
         {
             cout<<bufLine<<endl;
+            outFile<<bufLine<<endl;
             bufLine.clear();
             bufLine=nextLine;
         }
