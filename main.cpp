41c791a3ba04c98683175d2f021431e65eb739e7
StevenQiao <983466967@qq.com>
2018-10-02 19:31:23 +0800

    read second try

--- main.cpp
+++ main.cpp
@@ -1,16 +1,32 @@
 #include<iostream>
 #include<fstream>
 using namespace std;
+
+
+
 int main()
 {
     fstream inFile;
     inFile.open("../ast1.c.001t.tu",ios::in);
     string bufLine;
+    string nextLine;
+
+    getline( inFile,bufLine);//The firstLine
 
     while(!inFile.eof())
     {
-       getline( inFile,bufLine);
-       cout<<bufLine<<endl;
+       getline(inFile,nextLine);
+       if(nextLine[0]=='@')
+        {
+            cout<<bufLine<<endl;
+            bufLine.clear();
+            bufLine=nextLine;
+        }
+        else
+        {
+            bufLine=bufLine+" "+nextLine;
+            nextLine.clear();
+        }
     }
 
     inFile.close();
